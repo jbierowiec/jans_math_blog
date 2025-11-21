fe528eb1cb11a644069f8ac13b25938b7ec3a6d9
Jan Bierowiec <jbierowiec@fordham.edu>
2025-11-21 12:01:27 -0500

    UI & formatting updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,24 @@
 const { DateTime } = require("luxon");
 
-module.exports = function(eleventyConfig) {
-    
-    eleventyConfig.addPassthroughCopy('./src/style.css');
-    eleventyConfig.addPassthroughCopy('./src/assets');
-    eleventyConfig.addPassthroughCopy('./src/admin');
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/admin");
 
-    eleventyConfig.addFilter("postDate", (dateObj) => {
-        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
-    })
+  // Existing filter for blog post dates
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
 
-    return {
-        dir: {
-            input: "src",
-            output: "public"
-        }
-    };
-}
+  // NEW: simple filter that returns the current year as "yyyy"
+  eleventyConfig.addFilter("year", () => {
+    return DateTime.now().toFormat("yyyy");
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
